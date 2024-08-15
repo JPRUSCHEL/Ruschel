8917c0f2700e2042f8660a26382585741b2e43d3
JPRUSCHEL <joao.ruschel@iesb.edu.br>
2024-08-15 00:48:15 +0000

    Codigos da aula 1

---
+++ Aulas/aula01/sintaxe_c.c
@@ -0,0 +1,28 @@
+
+
+#include <stdio.h>     //inclusao de biblioteca
+
+int main() {          //funcao principal
+  //seu codigo aqui
+
+  //1.entrada
+  //levantar meus ingredientes
+  // char, int, float, double, long, long long, unsigned, unsigned char
+  //variaveis
+  //leitura
+  int primeiro_numero;
+  int segundo_numero;
+  primeiro_numero = 5;
+  segundo_numero = 6;
+  
+  
+  //2.processamento
+  // calculo soma, subtracao, multiplicacao, divisao, modulo
+  int soma = primeiro_numero + segundo_numero;
+  
+  //3.saida
+  //escrita
+  printf("A soma de %i com %i eh %i\n", primeiro_numero, segundo_numero, soma);
+  return 0;           // return fim do programa
+}
+
